fdb85bacb28b927c84e40d2525369073ada39289
Admin <admin@mac-admin.local>
2018-03-31 11:20:37 +0400

    Initial Commit

---
+++ Calculator_beta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator_beta
+//
+//  Created by Admin on 31.03.2018.
+//  Copyright © 2018 StscherbininaA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator_beta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator_beta
+//
+//  Created by Admin on 31.03.2018.
+//  Copyright © 2018 StscherbininaA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
